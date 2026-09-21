c69f58bb524b60041c2f43d3825049fd77ed505b
kowshikcseb-ship-it <kowshikcseb@gmail.com>
2026-09-21 10:19:53 +0100

    decorator named my_decorator is created and is invoked

---
+++ Day/day1/p5.py
@@ -0,0 +1,11 @@
+def my_decorator(func):
+    def wrapper():
+        print("before")
+        func()
+        print("after")
+    return wrapper
+@my_decorator
+def say_hello():
+    print("hello")
+say_hello()
+    
