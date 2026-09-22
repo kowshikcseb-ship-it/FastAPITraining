3f0c707d66f6c5465e82350a880d8a069cabfb12
kowshikcseb-ship-it <kowshikcseb@gmail.com>
2026-09-22 08:24:35 +0100

    adding multiple Routes

--- Day/day2/main.py
+++ Day/day2/main.py
@@ -3,3 +3,10 @@ app =FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "hello world","number":8}
+@app.get("/about")
+def about():
+    return {"page":"about", "authour":"koushik"}
+@app.get("/health")
+def health():
+    return {"status":"ok"}
+
